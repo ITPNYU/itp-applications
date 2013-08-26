bfa5178ec861c94f7b0b27adc44df7cfe01c7c2c
Steve Klise <sk@skli.se>
2013-08-25 22:19:04 -0400

    Experience selection and map population

--- src/controllers/experiences_controller.rb
+++ src/controllers/experiences_controller.rb
@@ -2,8 +2,13 @@ class Experiences < Sinatra::Base
   register Sinatra::BaseApp
 
   get '/' do
+    @posts = Post.all(:coords.not => nil)
+    erb :'experiences/index', layout: :layout_experiences
   end
 
   get '/:id' do
+  end
+
+  get '/new' do
   end
 end
